bd723cce7a5d0e74c044fa14db343f7b94a8a3da
lpawar1107 <lakhan1107@gmail.com>
2026-03-31 06:33:49 +0530

    Add PWA support: service worker, manifest updates, and meta tags

---
+++ public/service-worker.js
@@ -0,0 +1,62 @@
+const CACHE_NAME = 'farmlink-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/static/css/main',
+  '/static/js/main',
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache).catch(err => {
+        console.log('Cache addAll error:', err);
+      });
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).then((response) => {
+        if (!response || response.status !== 200 || response.type === 'error') {
+          return response;
+        }
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+        return response;
+      }).catch(() => {
+        return caches.match('/index.html');
+      });
+    })
+  );
+});
